4f3fc756d03c8a26a4f21f33d17808606b8630e7
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-01-13 13:02:21 -0800

    done

--- Students/Model/StudentController.swift
+++ Students/Model/StudentController.swift
@@ -8,10 +8,85 @@
 
 import Foundation
 
+enum TrackType: Int {
+    case none
+    case iOS
+    case Web
+    case UX
+}
+
+enum SortOptions: Int {
+    case firstName
+    case lastName
+}
+
 class StudentController {
     
+    // User/Apps/Students... returns URL or nil if path doesn't exist
     private var persistentFileURL: URL? {
         guard let filePath = Bundle.main.path(forResource: "students", ofType: "json") else { return nil }
         return URL(fileURLWithPath: filePath)
+    }
+    
+    var students: [Student] = []
+    
+    func filter(with trackType: TrackType, sortedBy sorter: SortOptions, completion: @escaping ([Student]) -> Void) {
+        
+        var updatedStudents: [Student]
+        
+        switch trackType {
+        case .iOS:
+            updatedStudents = students.filter { $0.course == "iOS" }
+        case .Web:
+            updatedStudents = students.filter { $0.course == "Web" }
+        case .UX:
+            updatedStudents = students.filter { $0.course == "UX" }
+        default:
+            // filter for none, or any other track type
+            updatedStudents = students
+        }
+        
+        if sorter == .firstName {
+            updatedStudents = updatedStudents.sorted { $0.firstName < $1.firstName }
+        } else {
+            updatedStudents = updatedStudents.sorted { $0.lastName < $1.lastName }
+        }
+        
+        completion(updatedStudents)
+        
+    }
+    
+    // Void = () // returns nothing Closurs need -> then Void or () to mean returns nothing
+    func loadFromPersistentStore(completion: @escaping ([Student]?, Error?) -> Void) {
+        
+        // background queue /  main queue
+        let bgQueue = DispatchQueue(label: "studentQueue", attributes: .concurrent)
+        
+        // do on different queue/thread
+        bgQueue.async {
+            
+            // access to default file manager object (Finder window)
+            let fm = FileManager.default
+            
+            //
+            guard let url = self.persistentFileURL, fm.fileExists(atPath: url.path) else {return}
+            
+            // takes data in url and stores in data if it does exist
+            do {
+                // collection of bits
+                let data = try Data(contentsOf: url)
+                
+                // Decodes from JSON into Swift
+                let decoder = JSONDecoder()
+                
+                // you should expect to see an array of Student objects
+                let students = try decoder.decode([Student].self, from: data)
+                self.students = students // decoded students from JSON = students array in this controller
+                completion(students, nil) // name of closure, nil means no error
+            } catch {
+                print("Error loading student data \(error)")
+            }
+            
+        }
     }
 }
--- Students/View Controllers/StudentsViewController.swift
+++ Students/View Controllers/StudentsViewController.swift
@@ -17,37 +17,72 @@ class StudentsViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     // MARK: - Properties
+    
+    private let studentController = StudentController()
+    
+    private var filteredAndSortedStudents: [Student] = [] {
+        // when it's set, it will run this code
+        didSet{
+            tableView.reloadData()
+        }
+    }
 
+    // happens on main queue unless specified
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.dataSource = self
+        tableView.dataSource = self // Connecting datasource of tableView to this VC
+    
+        studentController.loadFromPersistentStore { (students, error) in
+            if let error = error {
+                NSLog("Error: \(error)")
+                return
+            }
+            
+            // Take this block and make it run on main queue
+            DispatchQueue.main.async {
+                if let students = students {
+                    self.filteredAndSortedStudents = students
+                }
+            }
+        }
     }
     
     // MARK: - Action Handlers
     
     @IBAction func sort(_ sender: UISegmentedControl) {
+        updateDataSource()
     }
     
     @IBAction func filter(_ sender: UISegmentedControl) {
+        updateDataSource()
     }
     
     // MARK: - Private
     
     private func updateDataSource() {
         
+        let filter = TrackType(rawValue: filterSelector.selectedSegmentIndex) ?? .none
+        let sort = SortOptions(rawValue: sortSelector.selectedSegmentIndex) ?? .firstName
+    
+        studentController.filter(with: filter, sortedBy: sort) { (students) in
+            self.filteredAndSortedStudents = students
+        }
     }
 }
 
 extension StudentsViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return filteredAndSortedStudents.count // Changed from 0 to 1 and tested to see if we would just get 1 cell with Title and Detail
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "StudentCell", for: indexPath)
         
         // Configure cell
+        let aStudent = filteredAndSortedStudents[indexPath.row] // row of cell we're trying to build
+        cell.textLabel?.text = aStudent.name
+        cell.detailTextLabel?.text = aStudent.course
         
         return cell
     }
